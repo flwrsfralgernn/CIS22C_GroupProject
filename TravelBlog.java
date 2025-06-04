f5f7f4e27788c616180e7a52df5efcd254394709
winferd <50471045+flwrsfralgernn@users.noreply.github.com>
2025-06-03 21:45:10 -0700

    Created TravelBlog.java

---
+++ TravelBlog.java
@@ -0,0 +1,181 @@
+import java.text.DecimalFormat;
+
+public class TravelBlog {
+    /** Variables **/
+    private String title;
+    private String author;
+    // Dates will be formatted like "mm/dd/yyyy" and we can use string.split to access month, day, and year published
+    private String datePublished;
+    private Location location;
+    // price will be on a scale of 1-5, with 1 being least expensive and 5 being most expensive
+    private int price;
+
+    /** Constructors **/
+    public TravelBlog() {
+        this.title = "";
+        this.author = "";
+        this.datePublished = "01/01/2000";
+        this.location = new Location();
+        this.price = 1;
+    }
+    public TravelBlog(String title, String author, String datePublished, Location location, int price) {
+        this.title = title;
+        this.author = author;
+        this.datePublished = datePublished;
+        this.location = location;
+        this.price = price;
+    }
+    public TravelBlog(String title, String author, String datePublished, String city, String country, int price) {
+        this.title = title;
+        this.author = author;
+        this.datePublished = datePublished;
+        this.location = new Location(city, country);
+        this.price = price;
+    }
+    public TravelBlog(String title, String author, int month, int day, int year, Location location, int price) {
+        this.title = title;
+        this.author = author;
+        this.datePublished = String.format("%02d/%02d/%04d", month, day, year);
+        this.location = location;
+        this.price = price;
+    }
+    public TravelBlog(String title, String author, int month, int day, int year, String city, String country, int price) {
+        this.title = title;
+        this.author = author;
+        this.datePublished = String.format("%02d/%02d/%04d", month, day, year);
+        this.location = new Location(city, country);
+        this.price = price;
+    }
+
+
+    /** Accessors **/
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public String getDatePublished() {
+        return datePublished;
+    }
+    public String getMonthPublished() {
+        String[] date = datePublished.split("/");
+        return date[0];
+    }
+    public String getDayPublished() {
+        String[] date = datePublished.split("/");
+        return date[1];
+    }
+    public String getYearPublished() {
+        String[] date = datePublished.split("/");
+        return date[2];
+    }
+    public Location getLocation() {
+        return location;
+    }
+    public String getLocationCity() {
+        return location.getCityName();
+    }
+    public String getLocationCountry() {
+        return location.getCountryName();
+    }
+    public int getPrice() {
+        return price;
+    }
+
+    /** Setters **/
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public void setDatePublished(String datePublished) {
+        this.datePublished = datePublished;
+    }
+    public void setMonthPublished(int month) {
+        if (month < 1 || month > 12) {
+            throw new IllegalArgumentException("Month must be between 1 and 12.");
+        }
+        String[] date = datePublished.split("/");
+        this.datePublished = String.format("%02d/%s/%s", month, date[1], date[2]);
+    }
+    public void setDayPublished(int day) {
+        if (day < 1 || day > 31) {
+            throw new IllegalArgumentException("Day must be between 1 and 31.");
+        }
+        String[] date = datePublished.split("/");
+        this.datePublished = String.format("%s/%02d/%s", date[0], day, date[2]);
+    }
+    public void setYearPublished(int year) {
+        String[] date = datePublished.split("/");
+        this.datePublished = String.format("%s/%s/%04d", date[0], date[1], year);
+    }
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+    public void setLocation(String city, String country) {
+        this.location = new Location(city, country);
+    }
+    public void setLocationCity(String city) {
+        if (this.location == null) {
+            this.location = new Location(city, "America");
+        } else {
+            this.location.setCityName(city);
+        }
+    }
+    public void setLocationCountry(String country) {
+        if (this.location == null) {
+            this.location = new Location("San Jose", country);
+        } else {
+            this.location.setCountryName(country);
+        }
+    }
+    public void setPrice(int price) {
+        if (price < 1 || price > 5) {
+            throw new IllegalArgumentException("Price must be between 1 and 5.");
+        }
+        this.price = price;
+    }
+
+    /** Hash */
+    @Override
+    public int hashCode() {
+        int month = Integer.parseInt(getMonthPublished());
+        int day = Integer.parseInt(getDayPublished());
+        int year = Integer.parseInt(getYearPublished());
+        return month * day + year;
+    }
+}
+
+class Location {
+    /** Variables **/
+    private String cityName;
+    private String countryName;
+
+    /** Constructors **/
+    public Location() {
+        this.cityName = "";
+        this.countryName = "";
+    }
+    public Location(String cityName, String countryName) {
+        this.cityName = cityName;
+        this.countryName = countryName;
+    }
+
+    /** Accessors **/
+    public String getCityName() {
+        return cityName;
+    }
+    public String getCountryName() {
+        return countryName;
+    }
+
+    /** Setters **/
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+}
